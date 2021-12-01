ad745e2f21c8ff7366e82264ff0b47011f2d1c31
NK Pranesh <nkpranesh@gmail.com>
2021-12-01 19:21:01 +0530

    Node_modules1

--- front-end/src/components/locateUser.js
+++ front-end/src/components/locateUser.js
@@ -1,38 +1,38 @@
-import * as React from 'react';
-import {useState} from 'react';
-import {render} from 'react-dom';
-import MapGL, {GeolocateControl} from 'react-map-gl';
+import * as React from "react";
+import { useState } from "react";
+import { render } from "react-dom";
+import MapGL, { GeolocateControl } from "react-map-gl";
 import DarkMap from "../data/map.json";
 
-
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiY29ubmVjdGluZ3dvcmxkIiwiYSI6ImNrd2l3anlzdDB3bTAycG1kYXVlYnZtaXAifQ.fsAb70tSq-vGoIwjXuqreg'; // Set your mapbox token here
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiY29ubmVjdGluZ3dvcmxkIiwiYSI6ImNrd2l3anlzdDB3bTAycG1kYXVlYnZtaXAifQ.fsAb70tSq-vGoIwjXuqreg"; // Set your mapbox token here
 
 const geolocateStyle = {
   top: 0,
   left: 0,
-  margin: 10
+  margin: 10,
 };
-const positionOptions = {enableHighAccuracy: true};
+const positionOptions = { enableHighAccuracy: true };
 
-export default function App() {
+export default function App(props) {
   const [viewport, setViewport] = useState({
     latitude: 37.8,
     longitude: 96,
     zoom: 3,
     bearing: 0,
-    pitch: 0
+    pitch: 0,
   });
-  const viewportChangeHandle  = (e)=>{
-    setViewport(e)
-    this.props.locate.latitudeLongitudeUpdate(e.latitude,e.longitude)
-  }
+  const viewportChangeHandle = (e) => {
+    setViewport(e);
+    props.locate.latitudeLongitudeUpdate(e.latitude, e.longitude);
+  };
   return (
     <MapGL
       {...viewport}
       width="100%"
       height="100%"
       mapStyle="mapbox://styles/mapbox/dark-v9"
-      onViewportChange={ viewportChangeHandle}
+      onViewportChange={viewportChangeHandle}
       mapboxApiAccessToken={MAPBOX_TOKEN}
     >
       <GeolocateControl
