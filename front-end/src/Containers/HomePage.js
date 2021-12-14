5d06049873760ee6032f1b65a3b32f5eedb32330
Your Name <you@example.com>
2021-12-14 17:40:10 +0530

    projectdone

--- front-end/src/Containers/HomePage.js
+++ front-end/src/Containers/HomePage.js
@@ -1,22 +1,62 @@
 import React from "react";
+import Navbar from "../components/navbar";
 import { Link } from "react-router-dom";
-
+import HomePage1 from "../media/HomePage1.svg";
+import HomePage2 from "../media/HomePage2.svg";
+import HomePage3 from "../media/HomePage3.svg";
+import "../stylesheets/HomePage.css";
 class HomePage extends React.Component {
-  render() {
+  render() { 
     return (
-      <div>
-        <Link to="/login">
-          <button className="button">Login</button>
-        </Link>
-        <Link to="/signup">
-          <button className="button">Sign Up</button>
-        </Link>
-        <Link to="/userpage">
-          <button className="button">User Page</button>
-        </Link>
+    <div>
+         <Navbar parent={{ login: false, signup: false }} />
+         <div className="HPPart1">
+           <div className="HPBody1">
+            <div className="HPHeading">
+              CONNECTING WORLD
+            </div>
+            <div className="Hptext">
+              This is an online platform which helps you to get to know about your circle and helps you to contact them.
+            </div>
+            <Link to="/signup">
+            <button>
+              Get started
+            </button>
+            </Link>
+           </div>
+           <div className="HPImg1">
+             <img  src={HomePage1} alt="img" />
+           </div>
+         </div>
+         <div className="HPPart2">
+           <div className="HPImg2">
+            <img  src={HomePage2} alt="img" />
+           </div>
+           <div className="HPBody2">
+            <div className="HPHeading2">
+              Find people in your circle near to your location
+            </div>
+            <div className="Hptext2">
+              This website helps to capture your location and help you to find people near you whom you can contact later.
+            </div>
+           </div>
+         </div>
+         <div className="HPPart3">
+           <div className="HPBody1">
+            <div className="HPHeading3">
+              Want to find a Heart surgeon in your circle?
+            </div>
+            <div className="Hptext3">
+              You can find out the occupation of people in your circle and get to know the possible ways to conatct
+           </div>
+         </div>
+         <div className="HPImg3">
+              <img  src={HomePage3} alt="img" />
+           </div>
       </div>
+    </div>
     );
   }
 }
-
+ 
 export default HomePage;
